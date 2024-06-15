69f83e998d469bd36d4e6555a1efc756aa13fd56
Aryan Atri <aryan.36.atri@gmail.com>
2024-06-15 18:12:36 +0530

    corrected mapbox require

--- controllers/listings.js
+++ controllers/listings.js
@@ -1,5 +1,5 @@
 const Listing = require("../models/listing") ;
-const mbxGeocoding = require('@mapbox/mapbox-sdk/services/geoCoding');
+const mbxGeocoding = require('@mapbox/mapbox-sdk/services/geocoding');
 const mapToken = process.env.MAP_TOKEN ;
 const geocodingClient = mbxGeocoding({accessToken : mapToken});
 
